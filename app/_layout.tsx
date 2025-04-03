fed821353bdeb8d7eb7855c9b417866c9b9a3a77
Mayankpuri1000 <mayankpuri1000@gmail.com>
2025-04-03 23:55:07 +0530

    Mobile Movie App

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
+import React from "react";
 import { Stack } from "expo-router";
+import './globals.css'
+import { StatusBar } from "react-native";
+
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack>
+        <StatusBar hidden={true} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
+  
+
+  
+  
+  
+
 }
